81d202663e1c8cfb40e795f61c5f56d37bb5b97b
Hafizul12345 <82526547+Hafizul12345@users.noreply.github.com>
2022-06-17 10:36:58 +0800

    Update budget_tracker_v1.4.py

--- budget_tracker_v1.4.py
+++ budget_tracker_v1.4.py
@@ -5,66 +5,66 @@ Created on Thu Jun  9 09:44:56 2022
 @author: Muhamad Hafizul
 """
 
+from decimal import *
+TWOPLACE = Decimal(10) ** -2
+
 class Budget_Tracker:
     def __init__(self):
         self.source_income_dict = {}
         self.source_expense_dict = {}
-        self.total_income = 0
-        self.total_expense = 0
-        self.balance = 0
+        self.total_income = Decimal(0.00).quantize(TWOPLACE)
+        self.total_expense = Decimal(0.00).quantize(TWOPLACE)
+        self.balance = Decimal(0.00).quantize(TWOPLACE)
         
     def set_income(self,i_source,income):
-        income = int(income)
+        income = Decimal(income).quantize(TWOPLACE)
         self.source_income_dict |= {i_source:income}
         
     def set_expense(self,e_source,expense):
-        expense = int(expense)
+        expense = Decimal(expense).quantize(TWOPLACE)
         self.source_expense_dict |= {e_source:expense}
-        
-    def get_income_list(self):
-        print(f"{'Income source': <20} |{'Income value': <20} ")
-        print("-"*40)
-        for income in self.source_income_dict:
-            print(f"{income: <20} |RM {self.source_income_dict[income]: <20}")
-        print("\n")
-            
-    def get_expense_list(self):
-        print(f"{'Expense source': <20} |{'Expense list': <20} ")
-        print("-"*40)
-        for expense in self.source_expense_dict:
-            print(f"{expense: <20} |RM {self.source_expense_dict[expense]: <20}")
-        print("\n")
     
-    def get_total_income(self):
+    def set_total_income(self):
         for income in self.source_income_dict:
             self.total_income = (self.total_income 
                                 + self.source_income_dict[income])
-        print(f"Total income: RM {self.total_income}\n")
         
-    def get_total_expense(self):
+    def set_total_expense(self):
         for expense in self.source_expense_dict:
             self.total_expense = (self.total_expense 
                                   + self.source_expense_dict[expense])
-        print(f"Total expense: RM {self.total_expense}\n")
+            #print(self.total_expense)
     
     def set_balance(self):
         self.balance = self.total_income - self.total_expense
-        
-    def get_balance(self):
-        print(f"The balance: RM {self.balance}")
 
 #Test Run            
 hafizul = Budget_Tracker()
+
 hafizul.set_income("Salary",1980)
 hafizul.set_income("Parent",200)
-hafizul.get_income_list()
-hafizul.get_total_income()
+
+print(f"{'Income(s)': <20} |{'Value': <20}")
+for income in hafizul.source_income_dict:
+    print(f"{income: <20} |RM {hafizul.source_income_dict[income]: <20}")
+print("")
+
+hafizul.set_total_income()
+print(f"{'Total income': <20} :RM {hafizul.total_income}\n")
+
 hafizul.set_expense("Rent",450)
 hafizul.set_expense("Pant",600)
 hafizul.set_expense("Ticket",250)
 hafizul.set_expense("Food",400)
 hafizul.set_expense("Shopee",250)
-hafizul.get_expense_list()
-hafizul.get_total_expense()
+
+print(f"{'Expense(s)': <20} |{'Value': <20}")
+for expense in hafizul.source_expense_dict:
+    print(f"{expense: <20} |RM {hafizul.source_expense_dict[expense]: <20}")
+print("")
+
+hafizul.set_total_expense()
+print(f"{'Total expense': <20} :RM {hafizul.total_expense}\n")
+
 hafizul.set_balance()
-hafizul.get_balance()
+print(f"{'Total balance': <20} :RM {hafizul.balance}")
